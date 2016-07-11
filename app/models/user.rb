f3f7b4157f32f22eec0f855732a320048805badb
David Michalon <david.a.michalon@gmail.com>
2016-07-11 17:31:36 +0200

    associations for bookings and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+    has_many :demands, class_name: 'Booking', foreign_key: 'client_id'
+    has_many :missions, class_name: 'Booking', foreign_key: 'expert_id'
+
+
 end
