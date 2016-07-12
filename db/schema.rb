b1d6de46c57417f6b7c702ec5df0dcbb05497789
Jean-Baptiste Sebrier <jeanbaptiste.sebrier@gmail.com>
2016-07-12 17:49:56 +0200

    Profile page updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712091835) do
+ActiveRecord::Schema.define(version: 20160712131634) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +51,8 @@ ActiveRecord::Schema.define(version: 20160712091835) do
     t.string   "picture"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "city"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
