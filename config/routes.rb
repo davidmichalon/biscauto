9a4c201d5a168f2565f70c3ec12e9bc8db629b82
David Michalon <david.a.michalon@gmail.com>
2016-07-13 14:28:03 +0200

    dashboard via show back code

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
-  resources :users, only: [:show, :index, :new, :create] do
+  resources :users, only: [:show, :index, :edit, :update] do
     resources :bookings, only: [:create, :destroy, :new, :index]
   end
 
