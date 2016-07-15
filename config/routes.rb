247e9cbcdbcdc76133eeb1f58b33c5ba22538d29
David Michalon <david.a.michalon@gmail.com>
2016-07-15 17:15:51 +0200

    before the show

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     end
   end
 
+resources :profiles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your route
